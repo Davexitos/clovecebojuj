28deeedea1bdc66a69fcacb1d343d08209e0f1d0
Davexitos <dav1d.h3rout@gmail.com>
2026-01-23 17:44:31 +0100

    Inventory and geting gems

--- Scripts/board.gd
+++ Scripts/board.gd
@@ -2,8 +2,20 @@ extends Node2D
 enum players {blue,red,green,yelow}
 
 @export var ActivePlayer: players = players.blue
-@export var nextTurnLimit = false
+@export var nextTurnLimit = true
+@export var infinityRoll = false
+
+var getMage : bool = false
+var fight : bool = false
+
 var chance: Array[float] = [40,30,20,10]
+var inventory = [
+	#gem1-gem2-gem3-gem4-figure
+	[0,0,0,0,0], #plBlue
+	[0,0,0,0,0], #plRed
+	[0,0,0,0,0], #plGreen
+	[0,0,0,0,0]  #plYelow
+]
 
 var shift := [
 	[1,1],
@@ -45,9 +57,6 @@ var Map = [
 	[4,0,4,0,0,0,0,0,0,0,0,0,0,0,3,0,3]
 	]
 
-var Gems = []
-var Inventory = []
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -69,7 +78,7 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("NextTurn"):
-		nextTurn()
+		next()
 
 enum {X,Y}
 
@@ -110,19 +119,19 @@ func deleteMarks():
 	
 
 func nextTurn():
-	if GlobalVar.Roll > 0 and nextTurnLimit:
-		return
-	
 	GlobalVar.ActivePlayer += 1
 	rollDice()
-	deleteMarks()
-	activateMage()
+	updateInventory()
 	glowActons()
 	
 func _on_button_pressed() -> void:
-	nextTurn()
+	next()
 
 func rollDice():
+	if infinityRoll:
+		GlobalVar.Roll = 999
+		$Dice/Num.text = "X"
+		return
 	GlobalVar.Roll = randi_range(1,6)
 	$Dice/Num.text = str(GlobalVar.Roll)
 	
@@ -138,6 +147,7 @@ func move(PosX:int, PosY:int, subRoll: int):
 	
 
 func activateMage():
+	var toRemove: Array[Figure] = []
 	for x in Figures[4]:
 		var contact : int = 0 
 		for shif in shift:
@@ -153,14 +163,27 @@ func activateMage():
 						addGem(y)
 						break
 		if contact > 0:
-			Map[x.PosY][x.PosX] = 0
-			Figures[4].erase(x)
-			$Figure.remove_child(x)
+			toRemove.append(x)
+			
+	for i in toRemove:
+		Map[i.PosY][i.PosX] = 0
+		Figures[4].erase(i)
+		$Figure.remove_child(i)
 
 func addGem(val):
-	pass
+	inventory[GlobalVar.ActivePlayer][val] += 1
+	updateInventory()
+
+func updateInventory():
+	$Inventory/Gem1.text = str(inventory[GlobalVar.ActivePlayer][0])
+	$Inventory/Gem2.text = str(inventory[GlobalVar.ActivePlayer][1])
+	$Inventory/Gem3.text = str(inventory[GlobalVar.ActivePlayer][2])
+	$Inventory/Gem4.text = str(inventory[GlobalVar.ActivePlayer][3])
 
 func glowActons():
+	fight = false
+	getMage = false
+	
 	for i in $Glow.get_children():
 		$Glow.remove_child(i)
 		
@@ -171,3 +194,30 @@ func glowActons():
 				var newY = x.PosY+shif[Y]
 				if isInMap(newX, newY,false) and Map[newY][newX].Team != x.Team:
 					$"Glow".add_child(Glow.new(x.PosX,x.PosY,x.Team))
+					if x.Team == 4:
+						getMage = true
+					else:
+						fight = true
+	if getMage:
+		$Button/Label.text = "Collect"
+	elif fight:
+		$Button/Label.text = "Fight"
+	else:
+		$Button/Label.text = "Next Turn"
+
+func next():
+	deleteMarks()
+	if GlobalVar.Roll > 0 and nextTurnLimit:
+		return
+	elif getMage:
+		activateMage()
+		getMage = false
+	elif fight:
+		setFight()
+		fight = false
+	else:
+		nextTurn() 
+	glowActons()
+
+func setFight():
+	pass
